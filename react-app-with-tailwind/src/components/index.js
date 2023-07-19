e9b4fce26dc34b25fca1cf9f060837db57875955
Hamzamt99 <tamarihamza4@gmail.com>
2023-07-19 19:25:13 +0000

    install tailwind

---
+++ react-app-with-tailwind/src/components/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from './nav'
+export default function Home() {
+    return (
+        <div>
+            <header>
+                <Nav />
+            </header>
+        </div>
+    )
+}
